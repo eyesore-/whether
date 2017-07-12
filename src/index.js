b274b75724b4b6e90da59c8acf945915cbe6d8e1
Joel <joelbmolina@gmail.com>
2017-07-11 21:38:13 -0700

    added store to APP

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import location from './reducers/location'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
+let store = createStore(location)
+
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
 
