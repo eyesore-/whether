8f2060553085e45cb142cb422654e758da67a34a
Joel <joelbmolina@gmail.com>
2017-07-13 20:23:03 -0700

    creating get weather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,26 @@
 const GOOGLE_KEY = 'AIzaSyCziMqLH9SOOlx-xisQInYRQhzQT0sIGDc'
+const SKY_KEY = 'eebe3600259ca09c03f6e33ad9e2c6e1'
 const URI = {
+  weather: `https://api.darksky.net/forecast/${SKY_KEY}/`,
+  weatherOps: 'exclude=minutely?flags&units=auto',
   geolocate: 'https://www.googleapis.com/geolocation/v1/geolocate?key=',
   geocode: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=',
-  geocodeOptions: '&result_type=postal_code'
+  geocodeOps: '&result_type=postal_code',
+  proxy: 'https://cors-anywhere.herokuapp.com/'
+}
+
+const weather = (lat, lng) => {
+  let requestURL = `${URI.proxy + URI.weather}/${lat},${lng}?${URI.weatherOps}`
+  return fetch(requestURL).then(res => res.json())
 }
 
 const geocode = (lat, lng) => {
-  return fetch(`${URI.geocode}${lat},${lng}${URI.geocodeOptions}&key=${GOOGLE_KEY}`,
-    {method: 'GET'})
-    .then(res => res.json())
+  let requestURL = `${URI.geocode}${lat},${lng}${URI.geocodeOps}&key=${GOOGLE_KEY}`
+  return fetch(requestURL).then(res => res.json())
 }
 
 const geolocate = () => {
-  return fetch(`${URI.geolocate}${GOOGLE_KEY}`, {method: 'POST'})
+  return fetch(`${URI.geolocate}${GOOGLE_KEY}`, { method: 'POST' })
     .then(res => res.json())
     .then(data => geocode(data.location.lat, data.location.lng))
 }
@@ -23,13 +31,14 @@ export const getLocation = location => ({
 })
 
 export const getLocationAsync = () => dispatch => {
-  return geolocate()
-    .then(data => {
-      let locale = data.results[0]
-      dispatch(getLocation({
+  return geolocate().then(data => {
+    let locale = data.results[0]
+    dispatch(
+      getLocation({
         name: locale.address_components[1].short_name,
         latitude: locale.geometry.location.lat,
         longitude: locale.geometry.location.lng
-      }))
-    })
+      })
+    )
+  })
 }
