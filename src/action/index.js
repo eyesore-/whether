af0c14a4a5bb26bb2fb4300cd9f8d0774321ecbd
Joel <joelbmolina@gmail.com>
2017-07-20 19:14:42 -0700

    moved helper functions to proper file

--- src/action/index.js
+++ src/action/index.js
@@ -1,31 +1,4 @@
-const GOOGLE_KEY = 'AIzaSyCziMqLH9SOOlx-xisQInYRQhzQT0sIGDc'
-const SKY_KEY = 'eebe3600259ca09c03f6e33ad9e2c6e1'
-const URI = {
-  weather: `https://api.darksky.net/forecast/${SKY_KEY}`,
-  weatherOps: 'exclude=minutely?flags&units=auto',
-  geolocate: 'https://www.googleapis.com/geolocation/v1/geolocate?key=',
-  geocode: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=',
-  geocodeOps: '&result_type=postal_code',
-  proxy: 'https://cors-anywhere.herokuapp.com/'
-}
-
-const weather = (lat, lng) => {
-  let requestURL = `${URI.proxy + URI.weather}/${lat},${lng}?${URI.weatherOps}`
-  return fetch(requestURL)
-    .then(res => res.json())
-}
-
-const geocode = (lat, lng) => {
-  let requestURL = `${URI.geocode}${lat},${lng}${URI.geocodeOps}&key=${GOOGLE_KEY}`
-  return fetch(requestURL)
-    .then(res => res.json())
-}
-
-const geolocate = () => {
-  return fetch(`${URI.geolocate}${GOOGLE_KEY}`, { method: 'POST' })
-    .then(res => res.json())
-    .then(data => geocode(data.location.lat, data.location.lng))
-}
+import { geolocate, weather } from '../helper/util'
 
 export const getLocation = location => ({
   type: 'GET_LOCATION',
