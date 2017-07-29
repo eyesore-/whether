d5a674d16bfaf79e6b5568a8eeb53c1a1f2fe014
Joel <joelbmolina@gmail.com>
2017-07-28 21:31:12 -0700

    initial styling for hour container

--- src/component/time.jsx
+++ src/component/time.jsx
@@ -1,13 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import moment, { unix } from 'moment'
+import { unix } from 'moment'
 
 const Time = ({timestamp}) =>
   <div>
-    {unix(timestamp).date() === moment().date()
-      ? unix(timestamp).format('hh:mm A')
-      : unix(timestamp).format('ddd hh:mm A')
-    }
+    { unix(timestamp).format('hh:mm A') }
   </div>
 
 export default Time
