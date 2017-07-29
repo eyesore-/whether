cc045856988bdfcea43c5a4e83037e687cee6a1a
Joel <joelbmolina@gmail.com>
2017-07-28 21:39:06 -0700

    initial display styling

--- src/component/dayForecast.jsx
+++ src/component/dayForecast.jsx
@@ -4,12 +4,14 @@ import Temperature from './temperature'
 import Day from './day'
 
 const DayForecast = ({days}) =>
-  <div className="hourly-forecast">
+  <div className="daily-forecast">
     {days.data.map(day =>
-      <div className="hour" key={day.time}>
+      <div className="day" key={day.time}>
         <Day timestamp={day.time}></Day>
-        <Temperature temperature={day.temperatureMax}></Temperature>
-        <Temperature temperature={day.temperatureMin}></Temperature>
+        <div className="day-temp">
+          <Temperature temperature={day.temperatureMax}></Temperature>
+          <Temperature temperature={day.temperatureMin}></Temperature>
+        </div>
       </div>
     )}
   </div>
