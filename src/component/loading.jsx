03af9aa80b8e26f5696b421302633157ee788be7
Joel <joelbmolina@gmail.com>
2017-08-03 21:19:12 -0700

    created loading component

---
+++ src/component/loading.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Loading = () =>
+<div class="load-wrapper">
+  <div class="white ball"></div>
+  <div class="blue ball"></div>
+  <div class="yellow ball"></div>
+  <div class="red ball"></div>
+</div>
+
+export default Loading
